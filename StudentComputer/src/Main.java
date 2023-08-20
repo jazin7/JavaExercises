2f86a945bdae07445d8830de91284e4174c66afa
Jazin <70114019+jazin7@users.noreply.github.com>
2023-08-20 22:39:06 +0200

    test

---
+++ StudentComputer/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList test = new ArrayList();
+        test.add(1);
+        test.add("lol");
+        System.out.println(test);
+
+
+    }
+}
